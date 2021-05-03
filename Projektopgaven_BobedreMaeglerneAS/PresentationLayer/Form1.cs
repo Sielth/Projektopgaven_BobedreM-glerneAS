7084a1819ead37e0d864d79ab97204b1cd3bbd6b
Sielth <sielth@hotmail.it>
2021-05-03 18:08:31 +0200

    Bolig CRUD, valideringsmetoder Update

--- Projektopgaven_BobedreMaeglerneAS/PresentationLayer/Form1.cs
+++ Projektopgaven_BobedreMaeglerneAS/PresentationLayer/Form1.cs
@@ -26,5 +26,74 @@ namespace Projektopgaven_BobedreMaeglerneAS
             // Show the settings form
             boligUI.Show();
         }
+
+        // Initialize new Window when Opret Ny Bolig is selected from Form1 menu
+        private void opretNyToolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+            // Create a new instance of the BoligUI class
+            BoligUI boligUI = new BoligUI();
+
+            // Show the settings form
+            boligUI.Show();
+
+            // Disable not needed buttons
+            boligUI.GetHentBoligButton().Visible = false;
+            boligUI.GetOpdaterBoligButton().Visible = false;
+            boligUI.GetSletBoligButton().Visible = false;
+
+            // Disable not needed textboxes
+            boligUI.GetBoligIDTextbox().Enabled = false;
+            boligUI.GetBoligRenoveringsÅrDateTimePicker().Enabled = false;
+        }
+
+        // Initialize new Window when Hent Bolig is selected from Form1 menu
+        private void bolig_hentToolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+            // Create a new instance of the BoligUI class
+            BoligUI boligUI = new BoligUI();
+
+            // Show the settings form
+            boligUI.Show();
+
+            // Disable not needed buttons
+            boligUI.GetOpretBoligButton().Visible = false;
+            boligUI.GetOpdaterBoligButton().Visible = false;
+            boligUI.GetSælgBoligButton().Visible = false;
+            boligUI.GetSletBoligButton().Visible = false;
+        }
+
+        // Initialize new Window when Opdater Bolig is selected from Form1 menu
+        private void bolig_opdaterToolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+            // Create a new instance of the BoligUI class
+            BoligUI boligUI = new BoligUI();
+
+            // Show the settings form
+            boligUI.Show();
+
+            // Disable not needed buttons
+            boligUI.GetOpretBoligButton().Visible = false;
+            boligUI.GetHentBoligButton().Visible = false;
+            boligUI.GetSletBoligButton().Visible = false;
+
+            // Disable not needed TextBoxes
+            boligUI.GetBoligIDTextbox().Enabled = false;
+        }
+
+        // Initialize new Window when Slet Bolig is selected from Form1 menu
+        private void bolig_sletToolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+            // Create a new instance of the BoligUI class
+            BoligUI boligUI = new BoligUI();
+
+            // Show the settings form
+            boligUI.Show();
+
+            // Disable not needed buttons
+            boligUI.GetOpretBoligButton().Visible = false;
+            boligUI.GetHentBoligButton().Visible = false;
+            boligUI.GetOpdaterBoligButton().Visible = false;
+            boligUI.GetSælgBoligButton().Visible = false;
+        }
     }
 }
