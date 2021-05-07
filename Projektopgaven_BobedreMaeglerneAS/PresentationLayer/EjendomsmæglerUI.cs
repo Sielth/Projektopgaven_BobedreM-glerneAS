67ffc6be76af5170b6220e1ea6b8d46754a98219
tbni26606 <trine.berg.nissen@gmail.com>
2021-05-07 12:09:17 +0200

    Update EjendomsmæglerUI.cs

--- Projektopgaven_BobedreMaeglerneAS/PresentationLayer/EjendomsmæglerUI.cs
+++ Projektopgaven_BobedreMaeglerneAS/PresentationLayer/EjendomsmæglerUI.cs
@@ -23,10 +23,6 @@ namespace Projektopgaven_BobedreMaeglerneAS.PresentationLayer
 
         private void btn_OpretEjendomsmægler_Click(object sender, EventArgs e)
         {
-            //Connection string - 
-            string strconn = "Server=den1.mssql7.gear.host; Database=bobedredb; User ID=bobedredb; Password=Xw8gM?O3doQ_";
-            SqlConnection conn = new SqlConnection(strconn);
-
             EjendomsmæglerBLL ejendomsmæglerBLL = new EjendomsmæglerBLL(MælgerID(), MæglerCPR(), MæglerTelefon(), MæglerEmail(), MæglerFnavn(), MæglerEnavn(), MæglerVej(), MæglerPostnummer());
             EjendomsmæglerDAL ejendomsmæglerDAL = new EjendomsmæglerDAL(ejendomsmæglerBLL);
 
@@ -34,17 +30,13 @@ namespace Projektopgaven_BobedreMaeglerneAS.PresentationLayer
             ejendomsmæglerDAL.OpretEjendomsmægler(ejendomsmæglerBLL);
 
             //Loader data fra databasen ind i datagridview -
-            //EjendomsmæglerUI_Load(sender, e);
+            EjendomsmæglerUI_Load(sender, e);
         }
 
 
         //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         private void btn_HentEjendomsmægler_Click(object sender, EventArgs e) //NOT WORKING YET
         {
-            //Connection string - 
-            string strconn = "Server=den1.mssql7.gear.host; Database=bobedredb; User ID=bobedredb; Password=Xw8gM?O3doQ_";
-            SqlConnection conn = new SqlConnection(strconn);
-
             EjendomsmæglerBLL ejendomsmæglerBLL = new EjendomsmæglerBLL(MælgerID(), MæglerCPR(), MæglerTelefon(), MæglerEmail(), MæglerFnavn(), MæglerEnavn(), MæglerVej(), MæglerPostnummer());
             EjendomsmæglerDAL ejendomsmæglerDAL = new EjendomsmæglerDAL(ejendomsmæglerBLL);
 
@@ -69,16 +61,12 @@ namespace Projektopgaven_BobedreMaeglerneAS.PresentationLayer
             //ejendomsmæglerDAL.FindEjendomsmægler(ejendomsmæglerBLL);
 
             //Loader data fra databasen ind i datagridview
-            //EjendomsmæglerUI_Load(sender, e);
+            EjendomsmæglerUI_Load(sender, e);
         }
 
 
         private void btn_OpdaterEjendomsmægler_Click(object sender, EventArgs e)
         {
-            //Connection string - 
-            string strconn = "Server=den1.mssql7.gear.host; Database=bobedredb; User ID=bobedredb; Password=Xw8gM?O3doQ_";
-            SqlConnection conn = new SqlConnection(strconn);
-
             EjendomsmæglerBLL ejendomsmæglerBLL = new EjendomsmæglerBLL(MælgerID(), MæglerCPR(), MæglerTelefon(), MæglerEmail(), MæglerFnavn(), MæglerEnavn(), MæglerVej(), MæglerPostnummer());
             EjendomsmæglerDAL ejendomsmæglerDAL = new EjendomsmæglerDAL(ejendomsmæglerBLL);
 
@@ -86,16 +74,12 @@ namespace Projektopgaven_BobedreMaeglerneAS.PresentationLayer
             ejendomsmæglerDAL.OpdaterEjendomsmægler(ejendomsmæglerBLL);
 
             //Loader data fra databasen ind i datagridview
-            //EjendomsmæglerUI_Load(sender, e);
+            EjendomsmæglerUI_Load(sender, e);
         }
 
 
         private void btn_SletEjendomsmægler_Click(object sender, EventArgs e)
         {
-            //Connection string - 
-            string strconn = "Server=den1.mssql7.gear.host; Database=bobedredb; User ID=bobedredb; Password=Xw8gM?O3doQ_";
-            SqlConnection conn = new SqlConnection(strconn);
-
             EjendomsmæglerBLL ejendomsmæglerBLL = new EjendomsmæglerBLL(MælgerID(), MæglerCPR(), MæglerTelefon(), MæglerEmail(), MæglerFnavn(), MæglerEnavn(), MæglerVej(), MæglerPostnummer());
             EjendomsmæglerDAL ejendomsmæglerDAL = new EjendomsmæglerDAL(ejendomsmæglerBLL);
 
@@ -103,7 +87,7 @@ namespace Projektopgaven_BobedreMaeglerneAS.PresentationLayer
             ejendomsmæglerDAL.SletEjendomsmægler(ejendomsmæglerBLL);
 
             //Loader data fra databasen ind i datagridview
-            //EjendomsmæglerUI_Load(sender, e);
+            EjendomsmæglerUI_Load(sender, e);
         }
 
 
