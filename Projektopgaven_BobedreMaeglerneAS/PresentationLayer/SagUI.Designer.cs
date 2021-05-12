77c951741443cc603d705cffb4f6269ae96fa12f
Sielth <sielth@hotmail.it>
2021-05-12 11:43:05 +0200

    begin UI

--- Projektopgaven_BobedreMaeglerneAS/PresentationLayer/SagUI.Designer.cs
+++ Projektopgaven_BobedreMaeglerneAS/PresentationLayer/SagUI.Designer.cs
@@ -28,12 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.sagsnr_lbl = new System.Windows.Forms.Label();
+            this.sagsStatus_lbl = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // sagsnr_lbl
+            // 
+            this.sagsnr_lbl.AutoSize = true;
+            this.sagsnr_lbl.Location = new System.Drawing.Point(22, 23);
+            this.sagsnr_lbl.Name = "sagsnr_lbl";
+            this.sagsnr_lbl.Size = new System.Drawing.Size(43, 13);
+            this.sagsnr_lbl.TabIndex = 0;
+            this.sagsnr_lbl.Text = "Sags nr";
+            // 
+            // sagsStatus_lbl
+            // 
+            this.sagsStatus_lbl.AutoSize = true;
+            this.sagsStatus_lbl.Location = new System.Drawing.Point(22, 71);
+            this.sagsStatus_lbl.Name = "sagsStatus_lbl";
+            this.sagsStatus_lbl.Size = new System.Drawing.Size(37, 13);
+            this.sagsStatus_lbl.TabIndex = 1;
+            this.sagsStatus_lbl.Text = "Status";
+            // 
+            // SagUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.sagsStatus_lbl);
+            this.Controls.Add(this.sagsnr_lbl);
+            this.Name = "SagUI";
             this.Text = "SagUI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label sagsnr_lbl;
+        private System.Windows.Forms.Label sagsStatus_lbl;
     }
 }
