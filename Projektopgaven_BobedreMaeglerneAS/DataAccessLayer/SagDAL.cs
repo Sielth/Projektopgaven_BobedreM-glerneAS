c4aa21341f86a03ba102d2d38ccbb3aa9e2e43e2
Jon4dis <yuppst3r@gmail.com>
2021-05-12 13:38:58 +0200

    OpdaterSag og FindSag

--- Projektopgaven_BobedreMaeglerneAS/DataAccessLayer/SagDAL.cs
+++ Projektopgaven_BobedreMaeglerneAS/DataAccessLayer/SagDAL.cs
@@ -43,14 +43,78 @@ namespace Projektopgaven_BobedreMaeglerneAS.DataAccessLayer
             }
         }
 
-        public void FindSag(SagBLL sag)
+        public SagBLL FindSag(SagBLL sag)
         {
+            ConnectionSingleton s1 = ConnectionSingleton.Instance();
+            SqlConnection conn = s1.GetConnection();
 
+            string sqlCommandSag = "SELECT * FROM Sag WHERE SagsID = @SagsID";
+
+            SqlCommand commandSag = new SqlCommand(sqlCommandSag, conn);
+
+            commandSag.Parameters.AddWithValue("@SagID", sag.SagsID);
+            try
+            {
+                conn.Open();
+
+                using (SqlDataReader reader = commandSag.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        SagBLL matchingsag = new SagBLL((int)reader["SagsID"],
+                            reader["Status"].ToString(),
+                            (int)reader["BoligID"],
+                            (int)reader["SælgerID"],
+                            (int)reader["Mægler"]);
+
+                        return matchingsag;
+                    }
+                }
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return null;
         }
 
         public void OpdaterSag(SagBLL sag)
         {
+            ConnectionSingleton s1 = ConnectionSingleton.Instance();
+            SqlConnection conn = s1.GetConnection();
 
+            //ConnectionSingleton s1 = ConnectionSingleton.Instance();
+
+            //Tjekker om tekstboxe var tomme og undlader at opdaterer informationer for dem der er tomme
+            string sqlCommandSag = "UPDATE Sag SET" +
+                "Status = IsNull(NullIf(@Status, ''), Status)," +
+                "BoligID = IsNull(NullIf(@BoligID, ''), BolgID)," +
+                "SælgerID = IsNull(NullIf(@SælgerID, ''), SælgerID)," +
+                "MæglerID = IsNull(NullIf(@MæglerID, ''), MæglerID)," +
+                "WHERE SagsID = @SagsID";
+            //Sender input til database for at opdatere
+            SqlCommand cmdSag = new SqlCommand(sqlCommandSag);
+            cmdSag.Parameters.AddWithValue("@Status", sag.Status);
+            cmdSag.Parameters.AddWithValue("@BoligID", sag.BoligID);
+            cmdSag.Parameters.AddWithValue("@SælgerID", sag.SælgerID);
+            cmdSag.Parameters.AddWithValue("@MæglerID", sag.MæglerID);
+            try
+            {
+                conn.Open();
+                cmdSag.ExecuteNonQuery();
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                conn.Close();
+            }
         }
 
         public void SletSag(SagBLL sag)
