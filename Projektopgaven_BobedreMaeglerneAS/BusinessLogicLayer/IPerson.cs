5daeba8d50da9e2c4ada4db3641fb4c76d555fb7
tbni26606 <trine.berg.nissen@gmail.com>
2021-05-19 09:48:32 +0200

    IPerson

---
+++ Projektopgaven_BobedreMaeglerneAS/BusinessLogicLayer/IPerson.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projektopgaven_BobedreMaeglerneAS.BusinessLogicLayer
+{
+    interface IPerson
+    {
+        int CPR { get; set; }
+        int Telefon { get; set; }
+        string Email { get; set; }
+        string Fnavn { get; set; }
+        string Enavn { get; set; }
+        string Vej { get; set; }
+        int Postnummer { get; set; }
+    }
+}
